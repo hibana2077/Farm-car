8c92ffd4a979a9f52e2be9cfdff2bdc836f05ab2
hibana2077 <hibana2077@gmaill.com>
2023-05-15 16:14:44 +0800

    add models

--- test_model/models.py
+++ test_model/models.py
@@ -1,3 +1,11 @@
+'''
+Author: hibana2077 hibana2077@gmaill.com
+Date: 2023-05-15 16:04:31
+LastEditors: hibana2077 hibana2077@gmaill.com
+LastEditTime: 2023-05-15 16:14:34
+FilePath: /Farm-car/test_model/models.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 # import the necessary packages
 import torch
 from torchvision import models
