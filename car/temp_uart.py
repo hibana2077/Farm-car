4a03278b094889d2f7995144a76cad412e6d430b
hibana2077 <hibana2077@gmaill.com>
2023-05-21 20:29:51 +0800

    gg

--- car/temp_uart.py
+++ car/temp_uart.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2023-05-21 20:15:20
 LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2023-05-21 20:20:10
+LastEditTime: 2023-05-21 20:29:42
 FilePath: /Farm-car/car/temp_uart.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 
@@ -20,19 +20,6 @@ THS_list = ['/dev/ttyTHS1','/dev/ttyTHS2']
 S_list = ['/dev/ttyS0','/dev/ttyS1','/dev/ttyS2','/dev/ttyS3']
 
 for i in THS_list:
-    try:
-        print(f"Try {i}")
-        uart = serial.Serial(
-            port=i, 
-            baudrate=9600,
-            timeout=1
-        )
-        uart.write(b'Forwd 1000\r\n')
-        print(f"Try {i} done")
-        time.sleep(1)
-    except Exception as e:print(e)
-
-for i in S_list:
     try:
         print(f"Try {i}")
         uart = serial.Serial(
