993bd692764e119d357df1724c60b77d904fef92
hibana2077 <hibana2077@gmaill.com>
2023-05-21 20:04:16 +0800

    wtf

--- car/uart_contral.py
+++ car/uart_contral.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2023-05-20 19:49:22
 LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2023-05-21 20:02:51
+LastEditTime: 2023-05-21 20:04:09
 FilePath: /Farm-car/car/uart_contral.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -23,8 +23,5 @@ def forword(step,port,boudrate):
         ser.write(f"forword {step}".encode())
     logging.info(f"forword {step}")
 
-if __name__ == "__main__":
-    #get usb device list
-    ports = serial.tools.list_ports.comports()
-    for t in ports:
-        logging.info(t)
+ports = serial.tools.list_ports.comports()
+for t in ports:logging.info(t)
