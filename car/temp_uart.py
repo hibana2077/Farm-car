7b869bcb119509c32516de24000edf1992074d69
hibana2077 <hibana2077@gmaill.com>
2023-05-21 20:45:05 +0800

    add bytes size

--- car/temp_uart.py
+++ car/temp_uart.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2023-05-21 20:15:20
 LastEditors: hibana2077 hibana2077@gmaill.com
-LastEditTime: 2023-05-21 20:29:42
+LastEditTime: 2023-05-21 20:44:33
 FilePath: /Farm-car/car/temp_uart.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 
@@ -25,6 +25,7 @@ for i in THS_list:
         uart = serial.Serial(
             port=i, 
             baudrate=9600,
+            bytesize=serial.EIGHTBITS,
             timeout=1
         )
         uart.write(b'Forwd 1000\r\n')
